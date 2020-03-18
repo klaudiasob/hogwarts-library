fe98c86e5507b6b72836dbf859651298684a90e2
klaudiasob <61837479+klaudiasob@users.noreply.github.com>
2020-03-18 10:56:56 +0100

    Test (#4)

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -8,7 +8,10 @@ class GraphqlController < ApplicationController
     variables = ensure_hash(params[:variables])
     query = params[:query]
     operation_name = params[:operationName]
-    context = { current_user: current_user }
+    context = {
+        session: session,
+        current_user: current_user
+    }
     result = HogwartsLibrarySchema.execute(query, variables: variables, context: context, operation_name: operation_name)
     render json: result
   rescue => e
@@ -17,6 +20,16 @@ class GraphqlController < ApplicationController
   end
 
   private
+
+  def current_user
+    return unless session[:token]
+    crypt = ActiveSupport::MessageEncryptor.new(Rails.application.credentials.secret_key_base.byteslice(0..31))
+    token = crypt.decrypt_and_verify session[:token]
+    user_id = token.gsub('user-id:', '').to_i
+    User.find user_id
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+    nil
+  end
 
   # Handle form data, JSON body, or a blank value
   def ensure_hash(ambiguous_param)
--- app/graphql/hogwarts_library_schema.rb
+++ app/graphql/hogwarts_library_schema.rb
@@ -8,4 +8,9 @@ class HogwartsLibrarySchema < GraphQL::Schema
 
   # Add built-in connections for pagination
   use GraphQL::Pagination::Connections
+
+  def self.unauthorized_object(error)
+    # Add a top-level error to the response instead of returning nil:
+    raise GraphQL::ExecutionError, "An object of type #{error.type.graphql_name} was hidden due to permissions"
+  end
 end
